a637076b6843b59a04788c181070df8538b80b9f
erwin-io <erwinramirez220@gmail.com>
2024-09-10 12:28:35 +0800

    updates

--- web-client/src/environments/environment.prod.ts
+++ web-client/src/environments/environment.prod.ts
@@ -2,10 +2,19 @@ export const environment = {
   appName: "Shaj Flower shop",
   production: true,
   oneSignalAppId: "651fe52b-1a5e-4045-b722-5efb0c841f76",
-  apiBaseUrl: "http://philantro-link-api.vercel.app/api/v1",
+  apiBaseUrl: "http://shaj-flower-shop-api.vercel.app/api/v1",
   api: {
     auth: {
-      login: "/auth/login/customer"
+      login: "/auth/login/customer/",
+      registerCustomer: "/auth/register/customer/",
+      registerVerify: "/auth/register/verifyCustomer/",
+      resetSubmit: "/auth/reset/customerUserResetPasswordSubmit/",
+      resetVerify: "/auth/reset/customerUserVerify/",
+      resetPassword: "/auth/reset/customerUserResetPassword/",
+    },
+    users: {
+      getByCode: "/customer-user/",
+      updateProfile: "/customer-user/updateCustomerUserProfile/"
     }
   }
 };
